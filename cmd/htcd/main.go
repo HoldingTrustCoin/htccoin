97fa2735e4224009fb38b3d8ad08a3e84d6a2444
JVEdition <73716537+JVEdition@users.noreply.github.com>
2025-07-05 16:05:53 -0300

    feat: adiciona CLI do htcd

---
+++ cmd/htcd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/HoldingTrustCoin/htccoin/app"
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+)
+
+func main() {
+	rootCmd, _ := NewRootCmd()
+
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+		default:
+			os.Exit(1)
+		}
+	}
+}
